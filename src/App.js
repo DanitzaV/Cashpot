60cea1e5a35229e63d51542b6c10a029a3150f35
Cote <majovardon@gmail.com>
2018-10-01 23:44:32 -0400

    Login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB3CthvMNlvq5eb_JoKsil6RrZ82NXXEO4",
+    authDomain: "cashpot-68a8c.firebaseapp.com"
+})
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    state = { isSignedIn: false };
+
+    uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccess: () => false
+        }
+    }
+
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({ isSignedIn: !!user });
+        })
+    }
+
+    logOut = () => {
+        firebase.auth().signOut()
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.isSignedIn ? (
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-6 offset-3 text-center">
+                                <div>
+                                    <img class="mt-5" alt="foto" src={firebase.auth().currentUser.photoURL} />
+                                    <h1 class="mt-3">Bienvenida a la aplicación {firebase.auth().currentUser.displayName} </h1>
+                                    <button type="button" class="btn btn-success" onClick={this.logOut}>Salir</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>) : (
+                        <div className="container-fluid">
+                            <div class="row">
+                                <div class="col-6 offset-3 text-center">
+                                    <div class="container-fluid pt-5">
+                                        <StyledFirebaseAuth
+                                            uiConfig={this.uiConfig}
+                                            firebaseAuth={firebase.auth()} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from '../src/Login/Login';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
